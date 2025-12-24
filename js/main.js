a7d7cabbbe039fe9652fd7ccd14ce7fab6633588
Anubhab Pathak <anubhab01.aracnade@gmail.com>
2025-12-24 20:21:41 +0530

    feat: add link Java file and prepare Event data

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log("JavaScript connected");
+const events = [
+    {
+        title: "AI Hackathon",
+        date: "12 Feb 2025",
+        venue: "CU Auditorium"
+    },
+    {
+        title: "Web Development Bootcamp",
+        date: "18 Feb 2025",
+        venue: "Block C Seminar Hall"
+    },
+    {
+        title: "Startup Ideation Workshop",
+        date: "25 Feb 2025",
+        venue: "Innovation Lab"
+    }
+
+];
